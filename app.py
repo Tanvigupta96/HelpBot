523d05c620ed10f618c332963fde80d51dafa621
Tanvigupta96 <guptatannu.34gmail.com>
2019-06-21 01:47:12 +0530

    executed NewsBot

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from utils import fetch_reply
+import json
 
-from utils import fetch_reply 
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello, World!"
+    return "Hello, World!"
 
-@app.route("/sms", methods=[ 'POST' ])
+@app.route("/sms", methods=['POST'])
 def sms_reply():
-    """Respond to incoming calls with a single text message. """
+    """Respond to incoming calls with a simple text message."""
     # Fetch the message
+    print(json.dumps(request.form,indent = 2))
     msg = request.form.get('Body')
-    phone_no = request.form.get('From')
+    sender = request.form.get('From')
 
-    reply = fetch_reply(msg,phone_no)
-
-    #create reply
+    # Create reply
     resp = MessagingResponse()
-    resp.message(reply)
-
+    # resp.message("You said: {}".format(msg)).media("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
+    resp.message(fetch_reply(msg,sender))
     return str(resp)
 
 if __name__ == "__main__":
- 	app.run(debug=True)
- 	
+    app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "jokes-vssuhk-d2b213a1e739.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "client-secret.json"
 
 import dialogflow_v2 as dialogflow
 dialogflow_session_client = dialogflow.SessionsClient()
-PROJECT_ID = "jokes-vssuhk"
+PROJECT_ID = "tanvigupta96-ymmdnp"
+
+from gnewsclient import gnewsclient
+
+client = gnewsclient.NewsClient()
+
+def get_news(parameters):
+	print(parameters)
+	client.topic = parameters.get('news_type')
+	client.language = parameters.get('language')
+	client.location = parameters.get('geo-country')
+	return client.get_news()
 
 def detect_intent_from_text(text, session_id, language_code='en'):
     session = dialogflow_session_client.session_path(PROJECT_ID, session_id)
@@ -13,6 +24,20 @@ def detect_intent_from_text(text, session_id, language_code='en'):
     return response.query_result
 
 
-def fetch_reply(query, session_id):
-	response = detect_intent_from_text(query,session_id)
-	return response.fulfillment_text
+def fetch_reply(msg, session_id):
+	response = detect_intent_from_text(msg,session_id)
+
+	if response.intent.display_name == 'get_news':		
+		# return "news will be displayed! *{}*".format(dict(response.parameters))
+		news = get_news(dict(response.parameters))
+		news_str = 'Here are your *{}* news!'.format(client.topic)
+		for row in news:
+			news_str += "\n\n{}\n\n{}\n\n".format(row['title'],row['link'])
+		print(news_str)
+
+		
+		return news_str[:1100]
+		
+
+	else:
+		return response.fulfillment_text
